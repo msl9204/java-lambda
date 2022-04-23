45511bc3e1e8417125bfe8686a71533f90d359b1
minsoo.lee <letomok@gmail.com>
2022-04-24 01:09:42 +0900

    Comparator

---
+++ src/main/java/chapter4/ComparatorPractice.java
@@ -0,0 +1,24 @@
+package chapter4;
+
+import model.User;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class ComparatorPractice {
+
+    public static void main(String[] args) {
+        List<User> users = new ArrayList<>();
+        users.add(new User(3, "Alice"));
+        users.add(new User(1, "Charlie"));
+        users.add(new User(5, "Bob"));
+
+        Comparator<User> idComparator = (u1, u2) -> u1.getId() - u2.getId();
+        Collections.sort(users, idComparator);
+
+        Collections.sort(users, (u1, u2) -> u1.getName().compareTo(u2.getName()));
+        System.out.println(users);
+    }
+}
